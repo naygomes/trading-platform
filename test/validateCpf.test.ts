5237132f3e18ddd6c7d0c5aa97e595e1cdef3e2f
nayara.gomes <nayara.gomes@g.globo>
2025-07-19 23:11:06 -0300

    feat: making validateCpf function and its tests

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,25 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each(["97456321558", "71428793860", "87748248800"])(
+  "Deve testar um cpf válido: %s",
+  (cpf: string) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(true);
+  }
+);
+
+test.each(["974.563.215-58", "714.287.938-60", "877.482.488-00"])(
+  "Deve testar um cpf válido com formatação: %s",
+  (cpf: string) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(true);
+  }
+);
+
+test.each([null, undefined, "11111111111", "111", "1111111111111111"])(
+  "Deve testar um cpf inválido: %s",
+  (cpf: any) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(false);
+  }
+);
