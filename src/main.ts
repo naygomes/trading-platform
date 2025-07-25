807a7bf558cb1569a4f6c3688a780c2d3f43d716
nayara.gomes <nayara.gomes@g.globo>
2025-07-24 22:16:56 -0300

    feat: implement data validation for signup and enhance tests

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
-import express, { Request, Response } from "express";
-import crypto from "crypto";
-import pgp from "pg-promise";
+import express, { Request, Response } from 'express';
+import crypto from 'crypto';
+import pgp from 'pg-promise';
+import { validateName } from './validateName';
+import { validateEmail } from './validateEmail';
+import { validateCpf } from './validateCpf';
+import { validatePassword } from './validatePassword';
+import { Signup } from './interfaces';
 const app = express();
 app.use(express.json());
 
-const connection = pgp()("postgres://postgres:123456@db:5432/app");
+const connection = pgp()('postgres://postgres:123456@db:5432/app');
 
-app.post("/signup", async (req: Request, res: Response) => {
+function validateData(account: Signup): string | boolean {
+  if (!validateName(account.name)) {
+    return 'Invalid name';
+  }
+  if (!validateEmail(account.email)) {
+    return 'Invalid email';
+  }
+  if (!validateCpf(account.document)) {
+    return 'Invalid document';
+  }
+  if (!validatePassword(account.password)) {
+    return 'Invalid password';
+  }
+  return true;
+}
+
+app.post('/signup', async (req: Request, res: Response) => {
   const account = req.body;
+  const validationResult = validateData(account);
+  if (typeof validationResult === 'string') {
+    return res.status(400).json({ message: validationResult });
+  }
   const accountId = crypto.randomUUID();
   await connection.query(
-    "insert into trading_platform.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)",
+    'insert into trading_platform.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)',
     [accountId, account.name, account.email, account.document, account.password]
   );
   res.json({
@@ -18,18 +43,18 @@ app.post("/signup", async (req: Request, res: Response) => {
   });
 });
 
-app.get("/accounts", async (req: Request, res: Response) => {
+app.get('/accounts', async (req: Request, res: Response) => {
   const [account] = await connection.query(
-    "select * from trading_platform.account",
+    'select * from trading_platform.account',
     []
   );
   res.json(account);
 });
 
-app.get("/accounts/:accountId", async (req: Request, res: Response) => {
+app.get('/accounts/:accountId', async (req: Request, res: Response) => {
   const accountId = req.params.accountId;
   const [account] = await connection.query(
-    "select * from trading_platform.account where account_id = $1",
+    'select * from trading_platform.account where account_id = $1',
     [accountId]
   );
   res.json(account);
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
-test("Should create an aacount", async () => {
+const BASE_URL = 'http://localhost:3000';
+
+test('Should create an acount', async () => {
   // Given
   const input = {
-    name: "John Doe",
-    email: "john.doe@gmail.com",
-    document: "97456321558",
-    password: "asdQWE123",
+    name: 'John Doe',
+    email: 'john.doe@gmail.com',
+    document: '97456321558',
+    password: 'asdQWE123@',
   };
   // When
-  const responseSignup = await axios.post(
-    "http://localhost:3000/signup",
-    input
-  );
+  const responseSignup = await axios.post(`${BASE_URL}/signup`, input);
   const outputSignup = responseSignup.data;
   // Then
   expect(outputSignup.accountId).toBeDefined();
+
   const responseGetAccount = await axios.get(
-    `http://localhost:3000/accounts/${outputSignup.accountId}`
+    `${BASE_URL}/accounts/${outputSignup.accountId}`
   );
   const outputGetAccount = responseGetAccount.data;
   expect(outputGetAccount.name).toBe(input.name);
@@ -25,3 +25,54 @@ test("Should create an aacount", async () => {
   expect(outputGetAccount.document).toBe(input.document);
   expect(outputGetAccount.password).toBe(input.password);
 });
+
+test.each([
+  [
+    {
+      name: 'John_Doe',
+      email: 'john.doe@gmail.com',
+      document: '97456321558',
+      password: 'asdQWE123@',
+    },
+    'Invalid name',
+  ],
+  [
+    {
+      name: 'John Doe',
+      email: 'john@gmail',
+      document: '97456321558',
+      password: 'asdQWE123@',
+    },
+    'Invalid email',
+  ],
+  [
+    {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      document: '123',
+      password: 'asdQWE123@',
+    },
+    'Invalid document',
+  ],
+  [
+    {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      document: '97456321558',
+      password: 'senha123',
+    },
+    'Invalid password',
+  ],
+])(
+  'Should not create an account with invalid data: %p',
+  async (input, expectedMessage: string) => {
+    let responseError = {} as any;
+    try {
+      await axios.post(`${BASE_URL}/signup`, input);
+    } catch (error: Error | any) {
+      responseError = error.response;
+    }
+    expect(responseError.status).toBe(400);
+    expect(responseError.data.message).toBe(expectedMessage);
+  }
+);
